1791db6cc5a6223ab76c5adfd402f1524de3f2dd
hulyak <57311842+hulya95@users.noreply.github.com>
2020-10-06 17:42:54 -0700

    init firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCbKuG7VyI6jjs4-wGSA0UvQZK8ohBp0x4',
+  authDomain: 'chat-app-a2a87.firebaseapp.com',
+  databaseURL: 'https://chat-app-a2a87.firebaseio.com',
+  projectId: 'chat-app-a2a87',
+  storageBucket: 'chat-app-a2a87.appspot.com',
+  messagingSenderId: '327178180054',
+  appId: '1:327178180054:web:62a9b3eafeab2b9e328077',
+  measurementId: 'G-RNY6TT1FP1',
+};
+
+// instance of firebase app
+const app = firebase.initializeApp(config);
