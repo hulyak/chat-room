30ef1989dd9175f9ad0b94c66fe5b321ac735668
hulya <hulyakarakayaa06@gmail.com>
2020-11-06 23:01:35 -0800

    start building dashboard, add signout button and display user name

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,10 +1,18 @@
+import { auth } from 'firebase';
 import React from 'react'
-import { Button, Drawer } from 'rsuite'
+import { Alert, Button, Drawer } from 'rsuite'
 import Dashboard from '.';
 
 const DashboardToggle = () => {
     const {isOpen, close, open} = useModalState();
     const isMobile = useMediaQuery('(max-width: 992px)');
+
+    const onSignOut = useCallback(() => {  // memoize 
+        auth.signOut(); // firebase
+        Alert.info('Signed out', 4000);
+
+        close();
+    }, [close])
 
     return (
         <>
@@ -13,7 +21,7 @@ const DashboardToggle = () => {
           </Button>
           {/* modal toggler */}
           <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-            <Dashboard />
+            <Dashboard onSignOut={onSignOut} />
           </Drawer>
         </>
     )
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Drawer } from 'rsuite'
+import { useProfile } from '../../context/profile.context'
 
-const Dashboard = () => {
+const Dashboard = ({onSignOut}) => {
+    const {profile} = useProfile()
+
     return (
-        <div>
-            Hello
-        </div>
+        <>
+         <Drawer.Header>
+             <Drawer.Title>
+                Dashboard
+             </Drawer.Title>
+         </Drawer.Header>
+
+         <Drawer.Body>
+         <h3>Hey, {profile.name}</h3>
+
+         </Drawer.Body>
+
+         <Drawer.Footer>
+            <Button block color="red" onClick={onSignOut}>
+                Sign Out
+            </Button>
+         </Drawer.Footer>
+
+     </h3>
     )
 }
 
