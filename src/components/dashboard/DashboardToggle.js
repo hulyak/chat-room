36430d75316057ccc4f4d224a72620dacc7efe3a
hulya <hulyakarakayaa06@gmail.com>
2020-11-06 22:48:02 -0800

    add useMediaQuery hook and make drawer responsive

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -4,13 +4,15 @@ import Dashboard from '.';
 
 const DashboardToggle = () => {
     const {isOpen, close, open} = useModalState();
+    const isMobile = useMediaQuery('(max-width: 992px)');
 
     return (
         <>
           <Button block  color="blue" onClick={open}>
             <Icon icon="dashboard" />Dashboard
           </Button>
-          <Drawer show={isOpen} onHide={close} placement="left">
+          {/* modal toggler */}
+          <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
             <Dashboard />
           </Drawer>
         </>
--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,9 +1,32 @@
-const { useState } = require("react");
+const { useState, useCallback, useEffect } = require("react");
 
 export function useModalState(defaultValue = false) {
-    const [isOpen, setIsOpen] = useState(defaultValue)
-    const open = useCallback(() => setState(true) , []);
-    const close = useCallback(() => setState(false) , [])
+    const [isOpen, setIsOpen] = useState(defaultValue);
+
+    const open = useCallback(() => setIsOpen(true) , []);
+    const close = useCallback(() => setIsOpen(false) , [])
 
     return {isOpen, open, close}
 }
+
+// usage
+// const is992px = useMediaQuery('(max-width: 992px)')
+
+export const useMediaQuery = query => {
+    const [matches, setMatches] = useState(
+      () => window.matchMedia(query).matches
+    );
+  
+    useEffect(() => {
+      const queryList = window.matchMedia(query);
+      setMatches(queryList.matches);
+  
+      const listener = evt => setMatches(evt.matches);
+  
+      queryList.addListener(listener);
+      return () => queryList.removeListener(listener);
+    }, [query]);
+  
+    return matches;
+  };
+  
