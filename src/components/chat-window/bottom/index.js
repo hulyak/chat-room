2b8aaf0388f217a0dd7998f44efa797d20c1b761
hulya <hulyakarakayaa06@gmail.com>
2020-11-29 13:20:24 -0800

    create layout for chat page

---
+++ src/components/chat-window/bottom/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bottom = () => {
+    return (
+        <div>
+            Bottom
+        </div>
+    )
+}
+
+export default Bottom
--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router';
+import { Loader } from 'rsuite';
+import Messages from '../../components/chat-window/messages';
+import ChatTop from '../../components/chat-window/top';
+import ChatBottom from '../../components/chat-window/bottom';
+import { useRooms } from '../../context/rooms.context';
+
+
 
 const Chat = () => {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
+    // chat id parameter
+    const {chatId} = useParams();
+    const rooms = useRooms();
 
-export default Chat
+    if (!rooms){
+        return <Loader center vertical size="md" content="Loading" speed="slow" />
+    }
+
+    const currentRoom = rooms.find(room => room.id === chatId);
+
+    if(!currentRoom){
+        return <h6 className="text-center mt-page">Chat {chatId} not found</h6>
+    }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
+};
+
+export default Chat;
