3fce16a394aba87c1220a53246e80c15dea31e69
hulya <hulyakarakayaa06@gmail.com>
2020-11-28 21:13:08 -0800

    create the chat room list

---
+++ src/components/rooms/ChatRoomList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Nav } from 'rsuite'
+import RoomItem from './RoomItem'
+
+const ChatRoom = ({aboveElementHeight}) => {
+    return (
+        <Nav
+        appearance="subtle"
+        vertical
+        reversed
+        className="overflow-y-scroll custom-scroll"
+        style={{
+            height: `calc(100% - ${aboveElementHeight}px)`
+        }}
+        >
+        <Nav.Item>
+            <RoomItem />
+        </Nav.Item>
+            
+        </Nav>
+    )
+}
+
+export default ChatRoom
--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
+import { Divider } from 'rsuite'
 import CreateRoomBtnModal from '../components/CreateRoomBtnModal'
 import DashboardToggle from '../components/dashboard/DashboardToggle'
+import ChatRoomList from '../components/rooms/ChatRoomList'
 
 const Sidebar = () => {
+    const topSidebarRef= useRef();
+    const [height, setHeight] =useState(null);
+
+    useEffect(()=> {
+        if(topSidebarRef.current){
+            setHeight(topSidebarRef.current.scrollHeight)
+        }
+    }, [topSidebarRef]);
+
     return (
         <div className="h-100 pt-2">
-            <div>
+        {/* get the height of components */}
+            <div ref={topSidebarRef}>
                 <DashboardToggle />
                 <CreateRoomBtnModal />
+                <Divider>Join Conversation</Divider>
             </div>  
             {/* Chat room list*/}
+            <ChatRoomList aboveElementHeight={height} />
 
         </div>
     )
