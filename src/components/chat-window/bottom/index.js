09f391bbadf1aae580e013b7e99a1e3572433e17
hulya <hulyakarakayaa06@gmail.com>
2021-02-26 21:22:25 -0800

    create chat bottom send messages to the chat

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import firebase from 'firebase/app';
+import { useProfile } from '../../../context/profile.context';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
 
-const Bottom = () => {
-    return (
-        <div>
-            Bottom
-        </div>
-    )
+function assembleMessage(profile, chatId) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
 }
 
-export default Bottom
+const Bottom = () => {
+  const [input, setInput] = useState('');
+  const { profile } = useProfile();
+  const { chatId } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (input.trim() === '') {
+      return;
+    }
+    const msgData = assembleMessage(profile, chatId);
+    msgData.text = input;
+
+    const updates = {};
+    //   create last updates message,create messages in our database, get new unique key without creating the actual document
+    const messageId = database.ref('messages').push().key;
+    updates[`/messages/${messageId}`] = msgData;
+    updates[`rooms/${chatId}/lastMessage`] = {
+      ...msgData,
+      msgId: messageId,
+    };
+
+    setIsLoading(true);
+    try {
+      await database.ref().update(updates);
+      setInput('');
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message);
+    }
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      onSendClick();
+    }
+  };
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder="Write a new message here..."
+          value={input}
+          onChange={onInputChange}
+        />
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onSendClick}
+          disabled={isLoading}
+          onKeyDown={onKeyDown}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
+};
+
+export default Bottom;
