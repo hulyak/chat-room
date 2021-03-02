460847230f0bbeb96a631f9f507d3fffdffb624b
hulya <hulyakarakayaa06@gmail.com>
2021-03-01 19:33:32 -0800

    add an error boundary component and wrap our app inside this boundary

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
+import ErrorBoundary from './components/ErrorBoundary';
 
 import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <ProfileProvider>
-      <Switch>
-        <PublicRoute path="/signin">
-          <Signin />
-        </PublicRoute>
-        <PrivateRoute path="/">
-          <Home />
-        </PrivateRoute>
-      </Switch>
-    </ProfileProvider>
+    <ErrorBoundary>
+      <ProfileProvider>
+        <Switch>
+          <PublicRoute path="/signin">
+            <Signin />
+          </PublicRoute>
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </ProfileProvider>
+    </ErrorBoundary>
   );
 }
 
